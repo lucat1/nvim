9a83385fc85b24512c141d6ab23245a46d00f66d
Luca <lucat1@users.noreply.github.com>
2021-03-20 13:49:16 +0100

    attempt at fixing

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -3,3 +3,5 @@
 /bin/echo -e '\x1b[32mEnabling ethereal chroot-style...\x1b[0m'
 echo XBPS_CHROOT_CMD=ethereal >> ./void-packages/etc/conf
 echo XBPS_ALLOW_CHROOT_BREAKOUT=yes >> ./void-packages/etc/conf
+/bin/echo -e '\x1b[32mLinking / to /masterdir...\x1b[0m'
+ln -s / masterdir
