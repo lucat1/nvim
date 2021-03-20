67f789f7a5025a62459e310714eefff29966d280
Luca <lucat1@users.noreply.github.com>
2021-03-20 13:31:13 +0100

    make xbps-src run as root

---
+++ scripts/configure.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/bin/echo -e '\x1b[32mEnabling ethereal chroot-style...\x1b[0m'
+echo XBPS_CHROOT_CMD=ethereal >> ./void-packages/etc/conf
+echo XBPS_ALLOW_CHROOT_BREAKOUT=yes >> ./void-packages/etc/conf
